7445bc5d40fda509fe0fc99e22ff62729b2e1fd3
hochan <106953872+cheis11@users.noreply.github.com>
2025-08-25 12:46:23 +0900

    베이스 코드 및 애플리케이션 컨테이너화 복구

--- SprintMission7/src/main/java/com/sprint/mission/discodeit/dto/request/MessageUpdateRequest.java
+++
@@ -1,5 +0,0 @@
-package com.sprint.mission.discodeit.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record MessageUpdateRequest(@NotBlank String newContent) {}
--- SprintMission7/src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.mission.discodeit.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-import java.time.Instant;
-import java.util.UUID;
-
-public record ReadStatusCreateRequest(
-    @NotNull UUID userId, @NotNull UUID channelId, @NotNull Instant lastReadAt) {}
