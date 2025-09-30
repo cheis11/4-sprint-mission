3f999824d012557ce7eb43da0e3bd34b86d4ddb0
hochan <106953872+cheis11@users.noreply.github.com>
2025-09-30 10:23:24 +0900

    CSRF 보호 설정

---
+++ SprintMission9/src/main/java/com/sprint/mission/discodeit/controller/SecurityController.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@Slf4j
+public class SecurityController {
+
+  @GetMapping("csrf-token")
+  public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+    String tokenValue = csrfToken.getToken();
+    log.debug("CSRF 토큰 요청: {}", tokenValue);
+
+    return ResponseEntity
+        .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
+        .build();
+  }
+}
