c742d73745f7628757fff99ed80aaeb2107663a1
hochan <106953872+cheis11@users.noreply.github.com>
2025-10-22 22:19:50 +0900

    베이스 코드 설정

--- SprintMission10/src/main/java/com/sprint/mission/discodeit/security/Role.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.discodeit.security;
-
-public enum Role {
-  ADMIN,
-  CHANNEL_MANAGER,
-  USER
-}
--- SprintMission10/src/main/java/com/sprint/mission/discodeit/security/dto/JwtDto.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.security.dto;
-
-import com.sprint.mission.discodeit.dto.data.UserDto;
-
-public record JwtDto(
-    UserDto userDto,
-    String accessToken
-) {
-}
