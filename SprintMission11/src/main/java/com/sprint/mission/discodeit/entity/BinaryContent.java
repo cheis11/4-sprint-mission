8d0ca39b17f43104819b318988b3494a4a8c94ca
hochan <106953872+cheis11@users.noreply.github.com>
2025-11-03 18:19:40 +0900

    Update BinaryContent.java

--- SprintMission11/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ SprintMission11/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -4,6 +4,8 @@ import com.sprint.mission.discodeit.entity.base.BaseEntity;
 import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -21,6 +23,7 @@ public class BinaryContent extends BaseUpdatableEntity {
   private Long size;
   @Column(length = 100, nullable = false)
   private String contentType;
+  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private BinaryContentStatus status;
 
